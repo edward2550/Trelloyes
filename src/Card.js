99550dcae9312f873c36e0111fc47ea4164bd6b1
Edward Nunez <edwardnunez31@gmail.com>
2021-05-05 11:42:33 -0400

    first commit

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card(props) {
+  return (
+    <div className='Card'>
+      <button
+        type='button'
+        onClick={() => props.onClickDelete(props.id)}
+      >
+        delete
+      </button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  )
+}
+
+Card.propType ={
+  onClickDelete: () => {}
+}
